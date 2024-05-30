0b456aebe8a7f5ed806a00e08bd6bfcd4b46aec3
brno100 <44279085+brno100@users.noreply.github.com>
2024-05-30 22:19:39 +0200

    Add files via upload

---
+++ cloudshell.ps1
@@ -0,0 +1,13 @@
+﻿# Variables
+$resourceGroupName = "RG01"
+$appServicePlanName = "ASP-RG01-bb32"
+$location = "West Europe"
+$webAppName = "sysweb04"
+# Check if the App Service Plan exists
+$appServicePlan = Get-AzAppServicePlan -ResourceGroupName $resourceGroupName -Name $appServicePlanName -ErrorAction SilentlyContinue
+# If the App Service Plan does not exist, create it with Free tier
+if (-not $appServicePlan) {
+    $appServicePlan = New-AzAppServicePlan -ResourceGroupName $resourceGroupName -Location $location -Name $appServicePlanName -SkuTier "Free"
+}
+# Create a new Web App
+New-AzWebApp -ResourceGroupName $resourceGroupName -Name $webAppName -Location $location -AppServicePlan $appServicePlanName
